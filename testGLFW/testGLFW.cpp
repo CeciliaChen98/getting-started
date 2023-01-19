5e7ede3f3cc65652ff3ebf2565d6f4682266d748
Yue Chen <ychen1@goldengate.cs.brynmawr.edu>
2023-01-19 15:24:08 -0500

    print

--- testGLFW/testGLFW.cpp
+++ testGLFW/testGLFW.cpp
@@ -11,6 +11,7 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 {
    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
    {
+      std::cout<< "Pressed escape\n";
       glfwSetWindowShouldClose(window, GLFW_TRUE);
    }
 }
